a10f146e8d3f5cf63f5a8db993df73c79261cc90
Shadow Rider <72166869+AlexRaj1118@users.noreply.github.com>
2021-01-23 00:08:00 +0530

    Add files via upload

---
+++ src/components/stateless-components/userDetails.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+export default function userDetails() {
+  return (
+    <div className="user">
+      User Details
+      {/* <div className="order-item user__avatar"></div>
+      <div className="order-item user__name">
+        Jane <span className="surname">Appleseed</span>
+      </div>
+      <div className="order-item user__weight">
+        <h2>57</h2>kg
+      </div>
+      <div className="order-item user__height">
+        <h2>163</h2>cm
+      </div> */}
+    </div>
+  );
+}
